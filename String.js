2949c2b1e792faa9dc1eaaa543309d9ca6ecc001
sumit4622 <sumit.ray9299@gmail.com>
2024-09-29 12:16:43 +0545

    adding all the file

---
+++ String.js
@@ -0,0 +1,51 @@
+// console.log('this is sumit ray \n He study in bachlors')
+//printing the code in new line using \n
+
+console.log("con" + "cat" + "e" + "nate");
+//concatenate the word using  +
+
+let Apple = "fruit"
+console.log(`sumit like an ${Apple}`) 
+
+//concatenate this string ${} this format is know as literals template
+
+
+// comparing the two string
+
+const Name1 = "Ram pramod ray";
+const Name2 = "Ranjesh and sumitray";
+
+if(Name1 > Name2){
+    console.log(`The grater number is ${Name1}`)
+}
+else if(Name1 < Name2);{
+    console.log(`The grater number is ${Name2}`)
+};
+
+console.log(Name1.length < Name2.length); 
+
+
+const Name12 = "Sumit";
+const Name23 = "Sumita";
+
+if(Name12 > Name23){
+    console.log(`The grater number is ${Name12}`)
+}
+else if(Name12 < Name23);{
+    console.log(`The grater number is ${Name23}`)
+};
+
+ 
+// Here javaScipt compair with ASCI number which related to ther word in here M have (109) where N have (110) so for that reason the elseif is print.//
+
+
+let Roman = "America";
+let sumit = Roman;
+sumit = "India"
+
+console.log(Roman);
+console.log(sumit);
+
+
+
+console.log(true ? 1 : 2);
